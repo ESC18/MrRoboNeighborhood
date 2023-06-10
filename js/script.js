f926db963d3da2a89857dd42d37357784afdf9e6
ESC18 <ponderosaprintingus@gmail.com>
2023-06-09 17:49:44 -0700

    Add an upgrade in functionality and additional processes to determine numbers and their values.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
 //Business Logic
+/*function numberLoop () {
+    let numberAr = [];
+    for (let number = parseInt(document.getElementById("input").value);number != 0;number-=1) {
+        numberAr = [number];
 
-function numberLoop () {
-    let baseAr = [];
-    for (input = parseInt(document.getElementById("input").value);input != 0;input-=1) {
-        let push = input;
-        let string = push.toString();
-        console.log(string)
-        //if (string.includes("1")) {
-          //  console.log(string);
 
-            //string.replace("1","Hello"); 
-            //console.log(string);
-
-        //}
+        console.log(numberAr);
     }
 }
 window.onload = function () {
     let form = document.getElementById("form");
 
     form.addEventListener("submit", numberLoop);    
+}
+
+        //if (string.includes("1")) {
+          //  console.log(string);
+
+            //string.replace("1","Hello"); 
+            //console.log(string);
+
+        //}*/
+
+
+window.onload = function () {
+    numberAr = [];
+    let form = document.getElementById("form");
+    let output = document.getElementById("output").innerText;
+    form.addEventListener("submit", submitFunction);
+    function submitFunction(event) {
+        event.preventDefault();
+        let inputNumber = document.getElementById("input").value;
+        let savedNumber = inputNumber;
+        for (index = 0; index < inputNumber; index++) {
+            savedNumber = numberAr.push(index);
+            stringNumber = savedNumber.toString();
+            if (stringNumber.includes("1")) {
+                replaceNumber = stringNumber.replace("1","hello")
+                console.log(replaceNumber);
+            }
+            output = replaceNumber;
+        }
+    }
+
+
 }
