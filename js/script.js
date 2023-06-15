a8517cb87cc7a2f4888d03ea1609547fa20c3312
ESC18 <ponderosaprintingus@gmail.com>
2023-06-14 20:58:10 -0700

    Redo javascript code and add looping function for counting down as well as a tester function.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
-/*
- window onload: Adds event listener for submit - calls form and submit button
+function arraySave(savedValue) {
+    arr = [];
 
- 
-*/
+}
+
+
+
+
+
+
+
+
+function tester (index) {
+    if (index.toString().includes("1") === true) {
+        index = "Boop";
+        savedValue = index;
+        console.log(savedValue);
+        arraySave();
+    } else if (index.toString().includes("2") === true) {
+        index = "Beep";
+        savedValue = index;
+        console.log(savedValue);
+        arraySave();
+    } else if (index.toString().includes("3") === true) {
+        index = "Welcome to the Neighborhood"
+        savedValue = index;
+        console.log(savedValue);
+        arraySave();
+    } else {
+        savedValue = index;
+        console.log(savedValue);
+        arraySave();
+    }
+}
+
+function counter () {
+    let value = document.getElementById("input").value;
+    for (let index = 0; index <= value; index++) {
+        tester(index);
+    }
+}
+
+window.onload = function () {
+    let form = document.getElementById("form");
+    form.addEventListener("submit",counter);
+}
